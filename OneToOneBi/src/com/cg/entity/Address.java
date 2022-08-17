41ed8e2ab893028aa33093103f02f253b8f765d3
Dell <Dell@DESKTOP-MCK85GD>
2022-08-17 12:10:15 +0530

    first commit

---
+++ OneToOneBi/src/com/cg/entity/Address.java
@@ -0,0 +1,67 @@
+package com.cg.entity;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+
+	public class Address implements Serializable {
+	
+	private static final long SerialVersiononUID=1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int address_id;
+	
+	@Column(name="address_street")
+	private String street;
+	
+	@Column(name="address_city")
+	private String city;
+	
+	@Column(name="address_state")
+	private String State;
+	
+	@Column(name="address_zipcode")
+	private String zipcode;
+	
+	@OneToOne(mappedBy="address")
+	private Student student;
+	
+	public int getAddress_id() {
+		return address_id;
+	}
+	public void setAddress_id(int address_id) {
+		this.address_id = address_id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getZip() {
+		return zipcode;
+	}
+	public void setZip(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	}
---
+++ OneToOneBi/src/com/cg/entity/Client.java
@@ -0,0 +1,32 @@
+package com.cg.entity;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+	public static void main(String[] args) {
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+	EntityManager manager=factory.createEntityManager();
+	manager.getTransaction().begin();
+	
+	
+	Student student =new Student();
+	student.setName("Lekha");
+	Address address = new Address();
+	address.setStreet("xyz");
+	address.setState("Karnataka");
+	address.setCity("srt");
+	address.setZip("560098");
+	student.setAddress(address);
+	manager.persist(student);
+	manager.getTransaction().commit();
+	System.out.println("added one student details");
+	manager.close();
+	factory.close();
+	
+	
+	
+}
+}
